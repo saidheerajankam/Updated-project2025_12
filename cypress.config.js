bceb52d05d6c51c95acb47ae9fde47183a85a179
Sai Dheeraj <saidheerajankam@gmail.com>
2025-12-27 22:28:14 +0530

    Fix Cypress support file and config

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: "cypress/support/e2e.js",
+
     setupNodeEvents(on, config) {
-      on('task', {
-    logFilteredRequests(requests) {
-      return requests.filter(r => r.url.includes('/api'))
-    }
-  })
+      on("task", {
+        logFilteredRequests(requests) {
+          return requests.filter(r => r.url.includes("/api"));
+        }
+      });
     },
-    specPattern: 'cypress/e2e/**/*.js',
-    
-  },
+
+    specPattern: "cypress/e2e/**/*.js"
+  }
 });
