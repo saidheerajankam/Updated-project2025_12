e0c9d5fc2495218a42ca4c2b2805f8f3c6f85fba
Sai Dheeraj <saidheerajankam@gmail.com>
2025-12-28 20:44:17 +0530

    fix ci issue

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: "cypress/support/e2e.js",
     specPattern: "cypress/e2e/**/*.js"
   }
 });
