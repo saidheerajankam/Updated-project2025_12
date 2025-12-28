eb89708a126e1b5da74bc26ff78c76c39d3dc2ff
Sai Dheeraj <saidheerajankam@gmail.com>
2025-12-28 20:54:34 +0530

    disable cypress file for ci

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.js"
+    supportFile: false,
+    specPattern: "cypress/e2e/**/*.cy.js"
   }
 });
