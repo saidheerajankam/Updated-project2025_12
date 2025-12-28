64398e2b7ffdf1045cadee3ccc1ad9393081c567
Sai Dheeraj <saidheerajankam@gmail.com>
2025-12-28 20:16:24 +0530

    Fix cypress supportedfile path for CI

--- cypress.config.js
+++ cypress.config.js
@@ -3,15 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     supportFile: "cypress/support/e2e.js",
-
-    setupNodeEvents(on, config) {
-      on("task", {
-        logFilteredRequests(requests) {
-          return requests.filter(r => r.url.includes("/api"));
-        }
-      });
-    },
-
     specPattern: "cypress/e2e/**/*.js"
   }
 });
